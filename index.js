9cad14ca2decd53123c6afb335241a17993e882e
jacksolaro <jacksolaro@gmail.com>
2020-10-13 17:31:35 -0700

    Added base methods for viewing and adding employees, departments, and roles

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ connection.connect(function (err) {
     start();
 });
 
+
 // Start Function / Prompt Input
 function start() {
     inquirer
@@ -30,47 +31,213 @@ function start() {
             name: "choice",
             type: "rawlist",
             message: "What would you like to do?",
-            choices: ["Add Department, Role, or Employee", "View Departments, Roles, or Employees", "Update an Employees Role", "Bonus 1", "Bonus 2", "Bonus 3", "EXIT"],
+            choices: ["Add Department", "Add Employee", "Add Role", "View Departments", "View Employees", "View Roles", "Bonus 3", "EXIT"],
             loop: false
         })
         .then(function (answer) {
             // based on answer above, run function to retrive what user is looking for
             switch (answer.choice) {
-                case "Add Department, Role, or Employee":
+                case "Add Department":
                     //   Call Add Function
+                    addDepartment();
                     break;
-                case "View Departments, Roles, or Employees":
+                case "Add Employee":
                     //   Call View Function
+                    addEmployee();
                     break;
-                case "Update an Employees Role":
+                case "Add Role":
                     //   Call Update Function
+                    addRole();
                     break;
-                case "Bonus 1":
-                    //   Bonus 1
+                case "View Departments":
+                    //   Returns a table of all departments
+                    getDepartments();
                     break;
-                case "Bonus 2":
-                    //   Bonus 2
+                case "View Employees":
+                    //   Returns a table of all Employees
+                    getEmployees();
                     break;
-                case "Bonus 3":
-                    //   Bonus 3
+                case "View Roles":
+                    //   Returns a table of all roles
+                    getRoles();
                     break;
                 case "EXIT":
                     //   Exit Application
                     connection.end();
             }
         }).catch(error => {
-            if(error){
+            if (error) {
                 console.log(error)
             }
         });
 }
 
 // TODO: Add [DEPARTMENT, ROLE, EMPLOYEE]
+function addDepartment() {
+    // prompt for info about which song to look up
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "What is the name of the department?",
+            }
+        ])
+        .then(function (answer) {
+            // when finished prompting, return songs that match that song title
+            connection.query(`INSERT INTO department (name) VALUES (?)`, [answer.name], function (err, data) {
+                if (err) {
+                    throw err;
+                } else {
+                    getDepartments();
+                }
+            })
+        })
+}
 
+function addRole() {
+    connection.query(`SELECT * FROM department`, function (err, data) {
+        if (err) throw err;
+
+        let departmentArr = data.map(function (dep) {
+            return {
+                name: dep.name,
+                value: dep.id
+            }
+        })
+        // prompt for info about which song to look up
+        inquirer
+            .prompt([
+                {
+                    name: "title",
+                    type: "input",
+                    message: "What is the title of the Role?",
+                },
+                {
+                    name: "salary",
+                    type: "number",
+                    message: "What is the salary of the Role?",
+                },
+                {
+                    name: "department_id",
+                    type: "list",
+                    message: "Which department is this role in?",
+                    choices: departmentArr
+                }
+            ])
+            .then(function (answer) {
+                // when finished prompting, return songs that match that song title
+                connection.query(`INSERT INTO role (title,salary,department_id) VALUES (?,?,?)`, [answer.title,answer.salary,answer.department_id], function (err, data) {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.table(data);
+                        start();
+                    }
+                })
+            })
+    })
+}
+
+function addEmployee() {
+
+    connection.query(`SELECT * FROM role`, function (err, data) {
+        if (err) throw err;
+
+        let rolesArr = data.map(function (role) {
+            return {
+                name: role.title,
+                value: role.id
+            }
+        })
+
+        connection.query(`SELECT * FROM employee`, function (err, data) {
+            if (err) throw err;
+    
+            let employeeArr = data.map(function (employee) {
+                return {
+                    name: `${employee.first_name} ${employee.last_name}`,
+                    value: employee.id
+                }
+            })
+
+        // prompt for info about which song to look up
+        inquirer
+            .prompt([
+                {
+                    name: "first_name",
+                    type: "input",
+                    message: "What is employee's FIRST NAME?",
+                },
+                {
+                    name: "last_name",
+                    type: "input",
+                    message: "What is the employee's LAST NAME?",
+                },
+                {
+                    name: "role_id",
+                    type: "list",
+                    message: "Which role is this employee in?",
+                    choices: rolesArr
+                },
+                {
+                    name: "manager_id",
+                    type: "list",
+                    message: "Who is this employee's manager?",
+                    choices: employeeArr
+                }
+            ])
+            .then(function (answer) {
+                // when finished prompting, return songs that match that song title
+                connection.query(`INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)`, [answer.first_name,answer.last_name,answer.role_id,answer.manager_id], function (err, data) {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.table(data);
+                        start();
+                    }
+                })
+            })
+        })
+    })
+}
 
 
 // TODO: View [DEPARTMENT, ROLE, EMPLOYEE]
+function getDepartments() {
+    connection.query(`SELECT * FROM department`, function (err, data) {
+        if (err) {
+            throw err;
+        } else {
+            console.table(data);
+            start();
+        }
+    })
+}
 
+// TODO: View [DEPARTMENT, ROLE, EMPLOYEE]
+function getEmployees() {
+    connection.query(`SELECT * FROM employee`, function (err, data) {
+        if (err) {
+            throw err;
+        } else {
+            console.table(data);
+            start();
+        }
+    })
+}
+
+// TODO: View [DEPARTMENT, ROLE, EMPLOYEE]
+function getRoles() {
+    connection.query(`SELECT * FROM role`, function (err, data) {
+        if (err) {
+            throw err;
+        } else {
+            console.table(data);
+            start();
+        }
+    })
+}
 
 
 // TODO: Update Employees Role
--- schema.sql
+++ schema.sql
@@ -4,26 +4,29 @@ CREATE database acme_corpDB;
 USE acme_corpDB;
 
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
-  PRIMARY KEY (id)
---   FOREIGN KEY (role_id) REFERENCES department(id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT,
-  PRIMARY KEY (id)
---   FOREIGN KEY (role_id) REFERENCES role(id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
+
+INSERT INTO employee (first_name,last_name,role_id) VALUES ("Susan","Doe",1);
